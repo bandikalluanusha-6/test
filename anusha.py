455ffa25d4818ad6127af1a9df5f3a33615535a6
bandikalluanusha-6 <bandikallu.anusha0609@gmail.com>
2026-07-13 11:27:36 +0530

    Create anusha.py

---
+++ anusha.py
@@ -0,0 +1,14 @@
+Dear Beworld web services PTV LTD,
+Please find attached your invoice from AppsFlyer.
+Additional information regarding your bill and your account history are available under the payments tab on the dashboard.
+
+For Credit Card payment please,  CLICK HERE 
+
+Please be advised that we have updated our email address for all invoicing matters. This is an automated notification and cannot receive replies.
+Our new email address for invoice-related communications is invoicenoreply@appsflyer.com. We request that you update your records with this change to ensure continued smooth processing of your transactions. 
+
+For any questions or clarification, please contact us at accounting@appsflyer.com.
+
+Thank you for your business,
+Best Regards
+AppsFlyer Accounts Receivable Team
